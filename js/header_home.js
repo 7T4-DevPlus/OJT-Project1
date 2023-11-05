8533b53b02002996d63c3ef0e00fef01967a7ed3
LuanVo <vothanhluan19102001@gmail.com>
2023-11-05 09:25:09 +0700

    refactor navigation bar style

--- js/header_home.js
+++ js/header_home.js
@@ -5,14 +5,14 @@ const rightEl = document.querySelector(".header-right")
 
 leftEl.addEventListener("mouseover",function (){
     
-    headerEl.style.background = "no-repeat right top url(https://res.cloudinary.com/dfz0xsh2d/image/upload/v1699004034/Image_OJT7T4_Project1/Header_home/1_grxvjb.jpg)"
+    headerEl.style.background = "no-repeat right top url(https://res.cloudinary.com/dfz0xsh2d/image/upload/v1699114991/Image_OJT7T4_Project1/Header_home/blue_miacfu.jpg)"
     headerEl.style.backgroundSize = " cover";
     headerEl.style.backgroundPosition = "center";
 }) 
 
 rightEl.addEventListener("mouseover",function (){
     
-    headerEl.style.background = "no-repeat url(https://res.cloudinary.com/dfz0xsh2d/image/upload/v1699004034/Image_OJT7T4_Project1/Header_home/2_rjznt6.jpg)"
+    headerEl.style.background = "no-repeat url(https://res.cloudinary.com/dfz0xsh2d/image/upload/v1699114991/Image_OJT7T4_Project1/Header_home/z4849465503657_536a522980a5a23199bd1a8549b3502a_bywbva.jpg)"
     headerEl.style.backgroundSize = "cover";
     headerEl.style.backgroundPosition = "center";
 
