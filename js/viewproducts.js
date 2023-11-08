599f7bafb842b7bb4b4440aa029c1d09a00490a6
LuanVo <vothanhluan19102001@gmail.com>
2023-11-08 10:25:35 +0700

    fetch data from api

--- js/viewproducts.js
+++ js/viewproducts.js
@@ -17,3 +17,30 @@ productLink.onclick = function() {
         productList.style.display = 'none'; // Ẩn danh sách con
     }
 }
+
+async function getApiData() {
+    try {
+        const productApi = 'https://sevent4.onrender.com/api/products';
+        const productResponse = await fetch(productApi);
+        const products = await productResponse.json();
+
+        products.forEach(product => {
+            document.getElementById('products').innerHTML += `
+            <div class="product-box " style="height: 69vh; width: 18vw;">
+            <div class="product-thumbnail">
+                    <img class="original" src="${product.imgUrl[0]}">
+                    <img class="hover" src="${product.imgUrl[1]}">
+            </div>
+            <div class="product-info a-left">
+                <p class="product-name text2line" style="margin:0;">
+                ${product.name}</p>
+                <span class="price product-price">${product.price}<u>đ</u></span>
+            </div>       
+            </div>
+            `;
+        });
+    } catch (error) {
+        console.error('Lỗi khi gọi API:', error);
+    }
+}
+getApiData()
